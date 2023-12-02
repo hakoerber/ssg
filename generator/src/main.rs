7fa7387d178a8483c1d0c8e7ce978fbcb0e3bae3
Hannes Körber <hannes@hkoerber.de>
2023-12-02 20:23:35 +0100

    Set charset in HTML head

--- generator/src/main.rs
+++ generator/src/main.rs
@@ -101,6 +101,7 @@ fn frame(title: &str, inner: Markup, input_path: &Path) -> Markup {
                 link rel="stylesheet" href="/style.css" {}
                 link rel="icon" href="/favicon.svg"
                 script src="https://unpkg.com/htmx.org@1.9.9" {}
+                meta charset="utf-8" {}
                 meta name="viewport" content="width=device-width, initial-scale=1.0" {}
             }
             body hx-boost="true" {
